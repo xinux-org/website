225bc4e403995e685280152a991bab34af823cff
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-12 13:58:04 +0500

    uploading nextra initial configs

---
+++ README.md
@@ -0,0 +1,16 @@
+# [Xinux Docs](https://xinux.uz)
+
+Documentations for [Xinux](https://github.com/uzinfocom-org/xinux).
+
+The project uses [pnpm](https://pnpm.io), [Nextra](https://nextra.vercel.app) and deploys via [Vercel](https://vercel.com). To develop it locally, clone this repository and run the following command to start the local dev server:
+
+```bash
+pnpm install
+pnpm dev
+```
+
+And visit `localhost:3000` to preview your changes.
+
+## Contributing
+
+When making a change, or creating a new page, please make sure to edit all language files. You can simply copy the content of the edited English document (or the edited paragraph) and apply it to other language files. And then, volunteers are welcome to help with any untranslated sections.
---
+++ next.config.js
@@ -0,0 +1,57 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+});
+
+module.exports = withNextra({
+  i18n: {
+    locales: ["en-US", "ru", "uz"],
+    defaultLocale: "en-US",
+  },
+  redirects: () => {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        statusCode: 301,
+      },
+      {
+        source: "/advanced/performance",
+        destination: "/docs/advanced/performance",
+        statusCode: 301,
+      },
+      {
+        source: "/advanced/cache",
+        destination: "/docs/advanced/cache",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/cache",
+        destination: "/docs/advanced/cache",
+        statusCode: 301,
+      },
+      {
+        source: "/change-log",
+        destination: "/docs/change-log",
+        statusCode: 301,
+      },
+      {
+        source: "/blog/swr-1",
+        destination: "/blog/swr-v1",
+        statusCode: 301,
+      },
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        statusCode: 302,
+      },
+      {
+        source: "/examples",
+        destination: "/examples/basic",
+        statusCode: 302,
+      },
+    ];
+  },
+});

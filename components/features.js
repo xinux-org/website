af39ac5f1ba87a8e1dc8cb6dfa15f7588275fcf7
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-12 13:59:42 +0500

    components for page docs

---
+++ components/features.js
@@ -0,0 +1,131 @@
+import { useRouter } from "next/router";
+
+import styles from "./features.module.css";
+
+const Feature = ({ text, icon }) => (
+  <div className={styles.feature}>
+    {icon}
+    <h4>{text}</h4>
+  </div>
+);
+
+const TITLE_WITH_TRANSLATIONS = {
+  "en-US": "Uzbek Linux Community + Distribution",
+  ru: "Узбекское сообщество Linux + Дистрибуция",
+  uz: "O'zbek Linux Hamjamiyati + Distributivi",
+};
+
+// Translations for Features
+const FEATURES_WITH_TRANSLATIONS = {
+  "en-US": {
+    lightweight: "Lightweight",
+    realtime: "Fast",
+    typescript: "Productive",
+    remoteLocal: "Open Source",
+  },
+  uz: {
+    lightweight: "Yengil",
+    realtime: "Tezkor",
+    typescript: "Tayyor",
+    remoteLocal: "Open Source",
+  },
+  ru: {
+    lightweight: "Лёгкий",
+    realtime: "Быстро",
+    typescript: "Продуктивный",
+    remoteLocal: "Open Source",
+  },
+};
+
+export default () => {
+  const { locale, defaultLocale } = useRouter();
+
+  const featureText = (key) =>
+    FEATURES_WITH_TRANSLATIONS[locale]?.[key] ??
+    FEATURES_WITH_TRANSLATIONS[defaultLocale][key]; // Fallback for missing translations
+
+  return (
+    <div className="mx-auto max-w-full w-[880px] text-center px-4 mb-10">
+      <p className="text-lg mb-2 text-gray-600 md:!text-2xl">
+        {TITLE_WITH_TRANSLATIONS[locale]}
+      </p>
+      <div className={styles.features}>
+        <Feature
+          text={featureText("lightweight")}
+          icon={
+            <svg
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              stroke="currentColor"
+              strokeWidth="2"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z"></path>
+              <line x1="16" y1="8" x2="2" y2="22"></line>
+              <line x1="17.5" y1="15" x2="9" y2="15"></line>
+            </svg>
+          }
+        />
+        <Feature
+          text={featureText("realtime")}
+          icon={
+            <svg
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              shapeRendering="geometricPrecision"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z"></path>
+            </svg>
+          }
+        />
+        <Feature
+          text={featureText("typescript")}
+          icon={
+            <svg
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              shapeRendering="geometricPrecision"
+              viewBox="0 0 24 24"
+            >
+              <path d="M16.5 9.4l-9-5.19M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z"></path>
+              <path d="M3.27 6.96L12 12.01l8.73-5.05M12 22.08V12"></path>
+            </svg>
+          }
+        />
+        <Feature
+          text={featureText("remoteLocal")}
+          icon={
+            <svg
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              shapeRendering="geometricPrecision"
+              viewBox="0 0 24 24"
+            >
+              <circle cx="12" cy="12" r="2"></circle>
+              <path d="M16.24 7.76a6 6 0 010 8.49m-8.48-.01a6 6 0 010-8.49m11.31-2.82a10 10 0 010 14.14m-14.14 0a10 10 0 010-14.14"></path>
+            </svg>
+          }
+        />
+      </div>
+    </div>
+  );
+};

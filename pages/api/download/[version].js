8cfc60eb25cca961a894ba7f2a73079fced4672b
UwUssimo Robinson <uwussimo@icloud.com>
2022-09-12 09:57:34 +0500

    Update [version].js

--- pages/api/download/[version].js
+++ pages/api/download/[version].js
@@ -4,7 +4,7 @@ export default async (req, res)=> {
   switch (version) {
     case "latest":
       return res.redirect("https://mirror.dc.uz/xinux/iso/alpha/xinux-en-2022.08.09-x86_64.iso");
+    default:
+      return res.redirect("https://mirror.dc.uz/xinux/iso/alpha/xinux-en-2022.08.09-x86_64.iso");
   }
-
-  return res.json({message: "hello"})
 };

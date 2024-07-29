7f2b700d95939204b841aeaf01998a42288cbae5
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-29 20:56:33 +0500

    convert to mjs

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -1,12 +1,14 @@
+import nextra from 'nextra'
+
 const BASE_PATH = process.env.BASE_PATH;
 const PREFIX_PATH = "";
 
-const withNextra = require("nextra")({
+const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
 });
 
-module.exports = withNextra({
+export default withNextra({
   output: "export",
   images: {
     unoptimized: true,

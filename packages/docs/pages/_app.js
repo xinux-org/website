6dcdb6f91feaffb08d1d8145760d81be4dd25a93
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-29 21:16:26 +0500

    removing roots of amplitude

--- packages/docs/pages/_app.js
+++ packages/docs/pages/_app.js
@@ -1,44 +1,7 @@
 import "../styles.css";
-import amplitude from "amplitude-js";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  const router = useRouter();
-
-  const isBrowser = typeof window !== "undefined";
-  const [initialRouteTracked, setInitialRouteTracked] = useState(false);
-
-  useEffect(() => {
-    if (isBrowser && !initialRouteTracked) {
-      const event = "pageview";
-      const eventProperties = {
-        source: "docs",
-        path: router.asPath,
-      };
-      amplitude.getInstance().logEvent(event, eventProperties);
-      setInitialRouteTracked(true);
-    }
-  }, [router]);
-
-  useEffect(() => {
-    // Track page views
-    const handleRouteChange = (path) => {
-      const event = "pageview";
-      const eventProperties = {
-        source: "docs",
-        path: path,
-      };
-      amplitude.getInstance().logEvent(event, eventProperties);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, []);
 
   return getLayout(<Component {...pageProps} />);
 }

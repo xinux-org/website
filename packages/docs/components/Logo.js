6a1e4866ea6d7a5c7c02d8d2cd1ae396d6c5e8d0
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-29 21:09:56 +0500

    remap js

--- packages/docs/components/Logo.js
+++ packages/docs/components/Logo.js
@@ -9,27 +9,27 @@ export default function Logo() {
     >
       <defs>
         <linearGradient x1="21.3752273%" y1="96.8668474%" x2="53.9725479%" y2="23.928785%" id="linearGradient-1">
-          <stop stop-color="#699AD7" offset="0%"></stop>
-          <stop stop-color="#7EB1DD" offset="24.3%"></stop>
-          <stop stop-color="#7EBAE4" offset="100%"></stop>
+          <stop stopColor="#699AD7" offset="0%" />
+          <stop stopColor="#7EB1DD" offset="24.3%" />
+          <stop stopColor="#7EBAE4" offset="100%" />
         </linearGradient>
         <linearGradient x1="81.5341497%" y1="96.8674837%" x2="48.4794813%" y2="23.928785%" id="linearGradient-2">
-          <stop stop-color="#699AD7" offset="0%"></stop>
-          <stop stop-color="#7EB1DD" offset="24.3%"></stop>
-          <stop stop-color="#7EBAE4" offset="100%"></stop>
+          <stop stopColor="#699AD7" offset="0%" />
+          <stop stopColor="#7EB1DD" offset="24.3%" />
+          <stop stopColor="#7EBAE4" offset="100%" />
         </linearGradient>
         <linearGradient x1="75.2551762%" y1="0.652209878%" x2="43.236481%" y2="73.874063%" id="linearGradient-3">
-          <stop stop-color="#415E9A" offset="0%"></stop>
-          <stop stop-color="#4A6BAF" offset="23.2%"></stop>
-          <stop stop-color="#5277C3" offset="100%"></stop>
+          <stop stopColor="#415E9A" offset="0%"></stop>
+          <stop stopColor="#4A6BAF" offset="23.2%"></stop>
+          <stop stopColor="#5277C3" offset="100%"></stop>
         </linearGradient>
         <linearGradient x1="24.7445477%" y1="0.652209878%" x2="56.7631793%" y2="73.874063%" id="linearGradient-4">
-          <stop stop-color="#415E9A" offset="0%"></stop>
-          <stop stop-color="#4A6BAF" offset="23.2%"></stop>
-          <stop stop-color="#5277C3" offset="100%"></stop>
+          <stop stopColor="#415E9A" offset="0%" />
+          <stop stopColor="#4A6BAF" offset="23.2%" />
+          <stop stopColor="#5277C3" offset="100%" />
         </linearGradient>
       </defs>
-      <g id="Xinux-Logo" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="Xinux-Logo" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Group">
           <polygon id="Path" fill="url(#linearGradient-1)" points="501.946989 612 854 2.5162448 692.207329 1 598.2189 164.746663 503.557486 1.87475661 423.168807 1.90585907 382 72.9983011 516.861589 304.754375 421.12651 471.253605"></polygon>
           <polygon id="Path" fill="url(#linearGradient-2)" points="408.244609 447 156 -3.60805739e-14 312.05486 9.95118326e-14 489 306.337213"></polygon>
@@ -37,6 +37,6 @@ export default function Logo() {
           <polygon id="Path" fill="url(#linearGradient-4)" points="119.946409 2 472 611.483755 310.207062 613 216.218478 449.253337 121.560798 612.125243 41.1696526 612.094141 0 541.001699 134.861033 309.245625 39.1242407 142.742508"></polygon>
         </g>
       </g>
-    </svg>
+    </svg >
   );
 }

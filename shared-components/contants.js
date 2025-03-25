01f27ef67ba239b80f0dcb25accba1faa5299bdc
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-03-25 13:13:59 +0500

    chore: garbage constants + finished learn page

--- shared-components/contants.js
+++
@@ -1,4 +0,0 @@
-export const SANDBOX_DOCS_URL = "https://codesandbox.io/docs";
-export const PROJECTS_DOCS_URL = "https://codesandbox.io/docs/projects";
-export const VSCODE_DOCS_URL = "https://codesandbox.io/docs/vscode";
-export const IOS_DOCS_URL = "https://codesandbox.io/docs/ios";

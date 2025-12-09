c94af9fe68fdcb59ff56359cd8c052aa25e813bf
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-12-09 07:20:39 +0500

    fix: downloads page

--- components/Downloads.js
+++ components/Downloads.js
@@ -1,16 +1,42 @@
-import styles from "./Hero.module.css";
+import { useState, useEffect } from 'react';
+import { Cards, Card } from 'nextra/components'
+
+const normalize = (name) => {
+  const expression = new RegExp("");
+};
+
+function Download({
+  link,
+  error = "Xatolik yuz berdi!",
+  loading = "Yuklanmoqda...",
+  empty = "Hali %t chiqarilmaganga o'xshaydi",
+  type = "reliz"
+}) {
+  const [card, setCard] = useState(<Card title={loading} href="#" />);
+
+  useEffect(() => {
+    fetch(link)
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.length == 0) {
+          setCard(<Card title={empty.replace("%t", type)} href="#" />)
+        }
+
+        const pick = json[0].name;
+
+        setCard(<Card title={pick} href={`${link}/${pick}`} />)
+      })
+      .catch((_) => setCard(<Card title={error} href="#" />))
+  }, [])
+
+  return card
+}
 
 export default function Downloads({ title, link }) {
   return (
-    <div>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.subtitle}>{subtitle}</p>
-    </div>
-  );
+    <Cards>
+      <Download link="https://cdn.xinux.uz/release" />
+      <Download link="https://cdn.xinux.uz/latest" type="test" />
+    </Cards>
+  )
 }
-// <Cards>
-//   <Card title="Nostabil (25.11)" href={props.latestUrl}>
-//     {props.latestName}
-//   </Card>
-//   <Card title="Stabil (25.11) [Tez kunda]" href="https://cdn.xinux.uz/releases/" />
-// </Cards>

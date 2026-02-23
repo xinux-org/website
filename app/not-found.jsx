e86b522444cf959c325c7106f2403b23727ed812
Shuhrat Dehkanov <k@efir.uz>
2026-02-23 18:49:17 +0900

    fix: use "documentation" label

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import { NotFoundPage } from 'nextra-theme-docs'
 
 export default function NotFound() {
   return (
-    <NotFoundPage content="Bizga bu haqida xabar bering" labels="broken-link">
+    <NotFoundPage content="Bizga bu haqida xabar bering" labels="documentation">
       <h1>Sahifa topilmadi | 404</h1>
     </NotFoundPage>
   )

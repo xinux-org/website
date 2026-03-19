1e87815da9f1da73e0c8f4aadd842a9051207915
neo-unical <javohir@unical.uz>
2026-03-19 16:04:00 +0500

    fix: add download icon to cards in Downloads component

--- components/Downloads.js
+++ components/Downloads.js
@@ -2,6 +2,16 @@
 import { useState, useEffect } from "react";
 import { Cards } from "nextra/components";
 
+function DownloadIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4" />
+      <polyline points="7 10 12 15 17 10" />
+      <line x1="12" y1="15" x2="12" y2="3" />
+    </svg>
+  );
+}
+
 const normalize = (name, type) => {
   // 1 - name
   // 2 - version
@@ -19,23 +29,23 @@ function Download({
   empty = "Hali %t chiqarilmaganga o'xshaydi",
   type = "stabil",
 }) {
-  const [card, setCard] = useState(<Cards.Card title={loading} href="#" />);
+  const [card, setCard] = useState(<Cards.Card icon={<DownloadIcon />} title={loading} href="#" />);
 
   useEffect(() => {
     fetch(link)
       .then((res) => res.json())
       .then((json) => {
         if (json.length == 0) {
-          setCard(<Cards.Card title={empty.replace("%t", type)} href="#" />);
+          setCard(<Cards.Card icon={<DownloadIcon />} title={empty.replace("%t", type)} href="#" />);
         }
 
         const pick = json[0].name;
 
         setCard(
-          <Cards.Card title={normalize(pick, type)} href={`${link}/${pick}`} />,
+          <Cards.Card icon={<DownloadIcon />} title={normalize(pick, type)} href={`${link}/${pick}`} />,
         );
       })
-      .catch((_) => setCard(<Cards.Card title={error} href="#" />));
+      .catch((_) => setCard(<Cards.Card icon={<DownloadIcon />} title={error} href="#" />));
   }, []);
 
   return card;

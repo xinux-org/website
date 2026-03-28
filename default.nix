3bb9b2a82dbfa5c142ad710f94cac7599a945591
3nln <javohirtech@gmail.com>
2026-03-28 15:19:25 +0500

    fix: remove unused makeWrapper

--- default.nix
+++ default.nix
@@ -25,7 +25,6 @@ in
       nodejs_22
       pnpm.configHook
       typescript
-      makeWrapper
     ];
 
     buildInputs = with pkgs; [
@@ -64,7 +63,7 @@ in
       pname = manifest.name;
       version = manifest.version;
       src = source;
-      hash = "sha256-xKWfyWob2KC/B98gwqVVvbvDvhO6ro7g8qHRlmCSDI8=";
+      hash = "sha256-figV9UFI9nfagLbVJhY3k+Wi9v8K7zhmDkWD76EmCak=";
     };
 
     meta = with pkgs.lib; {
